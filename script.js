c325d59c35192b2b2a2ec9c5b24cf2b1e4f9bbae
Melanie-J-Baker <baker_mel@hotmail.com>
2022-09-09 15:42:25 +0100

    create function to limit size entered to 100. Change functions so that grid correctly resets when new number is entered

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 const grid = document.querySelector("#grid");
 const reset = document.querySelector("#reset");
 const changeSize = document.querySelector("#size");
-let size = prompt("What size would you like the sides of the grid to be?");
+let size = 0;
 let gridBoxes;
+
+function checkSize(size) {
+    size = prompt("What size would you like the sides of the grid to be?");
+    if(size >= 16 && size <= 100) {
+        createGrid(size);
+    } else if(size > 100) {
+        alert("Please select a number between 16 and 100.");
+    } else {
+        alert("Not a valid number. Please try again");
+    }
+}
 
 function createGrid(size) {
     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-
+    
     for(let i = 0; i < size * size; i++) {
         let div = document.createElement('div');
         div.classList.add("gridBox");
@@ -31,7 +42,7 @@ changeSize.addEventListener('click', (e) => {
     gridBoxes.forEach((gridBox) => {
         gridBox.classList.remove("active");
     });
-    size = prompt("What size would you like the sides of the grid to be?");
+    checkSize(size);
 });
 
-createGrid(size);
+checkSize(size);
